de2c96f5da4d5a3d157020df7353e3a11d0ff723
gr-temporary <gr-temporary@yandex.ru>
2018-09-29 19:25:37 +0700

    добавил jquery и плагин

---
+++ js/lib.js
@@ -0,0 +1,10 @@
+
+function happy(message) {
+	return message + ")))) 😂🤣😊";
+}
+
+module.exports = {
+	happy: happy,
+	count: 4,
+	smiles: ['😊','😃','✨','😜']
+};
---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var lib = require('./lib.js');
+var $ = require('jquery');
+var calendar = require('fullcalendar');
+
+console.log(lib.happy("Hello"));
+
+$('#calendar').fullCalendar({
+// put your options and callbacks here
+});
