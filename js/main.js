9c2cab293f132c47b49a3836857d92ef82e70f65
gr-temporary <gr-temporary@yandex.ru>
2018-10-01 10:16:54 +0700

    добавил readme

--- js/main.js
+++ js/main.js
@@ -3,8 +3,5 @@ var lib = require('./lib.js');
 var $ = require('jquery');
 var calendar = require('fullcalendar');
 
-console.log(lib.happy("Hello"));
 
-$('#calendar').fullCalendar({
-// put your options and callbacks here
-});
+console.log(lib.happy("Hello"));
